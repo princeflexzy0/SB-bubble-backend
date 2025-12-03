2f4947784ef36071e524b757c4e6b0bf772b82af
ATERE OLUWASEYI EMMANUEL <emmanuelatere44@gmail.com>
2025-12-03 12:53:04 +0000

    fix: Correct require paths in email.service.js

--- services/email.service.js
+++ services/email.service.js
@@ -1,6 +1,6 @@
 const nodemailer = require('nodemailer');
-const env = require('../config/env');
-const { createLogger } = require('../utils/logger');
+const env = require('./config/env');
+const { createLogger } = require('./utils/logger');
 const logger = createLogger('email-service');
 
 class EmailService {
